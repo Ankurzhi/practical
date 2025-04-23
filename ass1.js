0ec80abb5e7797202fa76a3aafeff93211c5dff3
Ankurzhi <132478524+Ankurzhi@users.noreply.github.com>
2025-04-23 14:21:54 +0530

    Add files via upload

---
+++ ass1.js
@@ -0,0 +1,11 @@
+function table()
+{
+    let a=parseInt(document.getElementById("tab").value);
+    for(i=1;i<=10;i++)
+    {
+        var c=a*1;
+        console.log(c);
+    }
+
+
+}
---
+++ day2.js
@@ -0,0 +1,12 @@
+// console.log("5"+4+5)
+// console.log("5"-"6")
+// console.log("5"+5+"4"+4+4)
+// console.log("5"/4+5)
+// function xyz()
+
+// {
+//     console.log("hello");
+    
+// }
+
+
